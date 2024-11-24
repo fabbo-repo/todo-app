0d367e52fe0631f4589da22ee4b85b374586ce38
Ivan <ivan7777gk@gmail.com>
2024-11-24 20:49:52 +0100

    [SPR JAVA API] Dockerfile + url mapping bugfix

--- api-spring-boot-java/src/main/java/com/fabbo/todoapp/modules/user/infrastructure/output/repositories/jpa/mappers/UserImageJpaMapper.java
+++ api-spring-boot-java/src/main/java/com/fabbo/todoapp/modules/user/infrastructure/output/repositories/jpa/mappers/UserImageJpaMapper.java
@@ -5,6 +5,7 @@ import com.fabbo.todoapp.modules.user.infrastructure.output.repositories.jpa.ent
 import com.fabbo.todoapp.modules.user.infrastructure.output.repositories.jpa.entities.UserJpaEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -15,7 +16,7 @@ public interface UserImageJpaMapper {
 
     @Mapping(source = "apiImage.id", target = "id")
     @Mapping(source = "apiImage.createdAt", target = "createdAt")
-    @Mapping(source = "apiImage.url", target = "url")
+    @Mapping(source = "apiImage.url", target = "url", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     @Mapping(source = "apiImage.urlExpiration", target = "urlExpiration")
     @Mapping(source = "user", target = "user")
     UserImageJpaEntity apiImageToUserImageEntity(
