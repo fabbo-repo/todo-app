2ba885984f281e16b95eb911994a1f9b0fdc6378
fabbo-repo <fscherle98@gmail.com>
2024-12-20 14:00:23 +0100

    [WEB REACT] Firebase env changes

--- web-react/src/common/config/firebase.ts
+++ web-react/src/common/config/firebase.ts
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app";
+import { FirebaseOptions, initializeApp } from "firebase/app";
 import {
   browserLocalPersistence,
   getAuth,
   setPersistence,
 } from "firebase/auth";
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY!,
-  authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID!}.firebaseapp.com`,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN!,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID!,
-  storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID!}.appspot.com`,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET!,
   messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID!,
   appId: import.meta.env.VITE_FIREBASE_APP_ID!,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID!,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
